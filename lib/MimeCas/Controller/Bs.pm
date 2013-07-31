457cca66900f066f90675fd141f1102c5eddd025
Henry Van Styn <vanstyn@intellitree.com>
2013-07-31 19:02:43 -0400

    CmsDemo: bs redirect limited to direct browser requests

--- lib/MimeCas/Controller/Bs.pm
+++ lib/MimeCas/Controller/Bs.pm
@@ -6,7 +6,20 @@ BEGIN {extends 'Catalyst::Controller'; }
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-    return $c->response->redirect('/tple/bs/section/Home.html');
+    
+    my $url = '/tple/bs/section/Home.html';
+    
+    return $c->response->redirect($url)
+      unless $c->req->header('X-RapidApp-RequestContentType');
+    
+    # Show the redirect target as a link in the ExtJS client:
+    # need to do this because the Ajax request will act on the redirect!
+    $c->response->content_type('text/html; charset=utf8');
+    $c->response->body(join('',
+      '<b>','302 Redirect: ',
+      '<a href="',$url,'">',$url,'</a></b>'
+    ));
+    return $c->detach;
 }
 
 __PACKAGE__->meta->make_immutable;
